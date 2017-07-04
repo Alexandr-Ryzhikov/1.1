137e22c2ac34245b9e494f74eb4191c1db8719d8
fuckingithub <raycalmar@gmail.com>
2017-07-04 17:47:16 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r=20;
+            Console.WriteLine("Введите внешний радиус (Больше 20): ");
+            Console.WriteLine("Площадь кольца S={0}", Math.PI * (Math.Pow(Convert.ToDouble(Console.ReadLine()), 2) - Math.Pow(r, 2)));
+            Console.ReadKey();
+        }
+    }
+}
